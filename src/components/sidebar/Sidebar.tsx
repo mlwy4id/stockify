540b513d9319932c801f1bb5e86d450f3dce2368
mlwy4id <azmiimaulawiysaid67@gmail.com>
2025-12-18 07:57:14 +0800

    feat: fix sidebar semantic

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -4,19 +4,23 @@ import { LuChartColumn } from "react-icons/lu";
 
 const Sidebar = () => {
   return (
-    <nav
+    <aside
       className="
         bg-gray-50 pt-5 flex flex-col gap-2
         h-screen sticky w-[20%] top-0
         shadow-md border border-gray-200
     "
     >
-      <h1 className="font-semibold text-2xl mx-2 pb-4 border-b border-b-gray-200 ">Stockify</h1>
-      <ul>
-        <SidebarOptions icon={LuArchive} name="Inventory" />
-        <SidebarOptions icon={LuChartColumn} name="Reports" />
-      </ul>
-    </nav>
+      <nav>
+        <h1 className="font-semibold text-2xl mx-2 pb-4 border-b border-b-gray-200 ">
+          Stockify
+        </h1>
+        <ul className="mt-2">
+          <SidebarOptions icon={LuArchive} name="Inventory" />
+          <SidebarOptions icon={LuChartColumn} name="Reports" />
+        </ul>
+      </nav>
+    </aside>
   );
 };
 
