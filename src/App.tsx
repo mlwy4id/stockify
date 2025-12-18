5d576214fc59a0d40b882fde114c3f2e13593aac
mlwy4id <azmiimaulawiysaid67@gmail.com>
2025-12-18 08:29:22 +0800

    feat: add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import Sidebar from "./components/sidebar/Sidebar";
 import Inventory from "./pages/Inventory";
 import Reports from "./pages/Reports";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="h-screen flex">
       <Sidebar />
 
-      <div className="overflow-y-auto w-screen min-h-screen">
+      <div className="overflow-y-auto px-5 w-screen min-h-screen">
+        <Navbar/>
+        
         <Routes>
           <Route path="/inventory" element={<Inventory />} />
           <Route path="/reports" element={<Reports />} />
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import { LuCircleUser, LuBell, LuSearch } from "react-icons/lu";
+import { Input } from "./ui/input";
+
+const Navbar = () => {
+  return (
+    <header className="py-4 pr-2 sticky top-0 bg-white">
+      <nav>
+        <ul className="flex justify-between items-center">
+          <div className="w-[20%] relative">
+            <Input className="w-full h-full py-1" placeholder="Search..." />
+            <LuSearch className="absolute top-[23%] right-[5%]" />
+          </div>
+          <div className="flex gap-4 items-center">
+            <LuBell size={24} />
+            <LuCircleUser size={24} />
+          </div>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
