37a9a6591fa8a268f38a4bd0deec907d9d3887b8
mlwy4id <azmiimaulawiysaid67@gmail.com>
2026-01-01 07:49:47 +0800

    fix: optimize create and update queries

--- src/hooks/queries/inventory.query.ts
+++ src/hooks/queries/inventory.query.ts
@@ -1,12 +1,17 @@
 import { createItem, deleteItem, getAllItems, getItem, updateItem } from '@/lib/api/inventory.api';
-import type { UpdateItemRequest } from '@/types/inventory';
+import type { Item, UpdateItemRequest } from '@/types/inventory';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useInventoryPathNavigation } from '../useInventoryPathNavigation';
+
+type UpdateContext = {
+  previousItems?: Item[];
+};
 
 export const useGetInventoryItems = () => {
   return useQuery({
     queryKey: ['Items'],
     queryFn: getAllItems,
+    staleTime: 1000 * 30,
   });
 };
 
@@ -23,12 +28,23 @@ export const useCreateInventoryItem = () => {
 
   return useMutation({
     mutationFn: createItem,
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: ['Items'],
-      });
+    onMutate: async (newItem) => {
+      // optimistic updates
+      await queryClient.cancelQueries({ queryKey: ['Items'] });
+      const previousItems = queryClient.getQueryData(['Items']);
+      queryClient.setQueryData(['Items'], (old: Item[]) => [...old, newItem]);
+      toInventory();
+
+      return { previousItems };
     },
-    onSettled: () => toInventory(),
+    onError: (_err, _newItem, context) => {
+      if (context?.previousItems) {
+        queryClient.setQueryData(['Items'], context.previousItems);
+      }
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['Items'] });
+    },
   });
 };
 
@@ -36,14 +52,27 @@ export const useUpdateInventoryItem = () => {
   const queryClient = useQueryClient();
   const { toInventory } = useInventoryPathNavigation();
 
-  return useMutation<unknown, Error, UpdateItemRequest>({
+  return useMutation<unknown, Error, UpdateItemRequest, UpdateContext>({
     mutationFn: updateItem,
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: ['Items'],
-      });
+    onMutate: async (updatedItem) => {
+      await queryClient.cancelQueries({ queryKey: ['Items'] });
+      const previousItems = queryClient.getQueryData<Item[]>(['Items']);
+
+      queryClient.setQueryData<Item[]>(['Items'], (old = []) =>
+        old.map((item) => (item.id === updatedItem.id ? { ...item, ...updatedItem } : item))
+      );
+
+      return { previousItems };
     },
-    onSettled: () => toInventory(),
+    onError: (_err, _updatedItem, context) => {
+      if (context?.previousItems) {
+        queryClient.setQueryData(['Items'], context.previousItems);
+      }
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['Items'] });
+      toInventory();
+    },
   });
 };
 
