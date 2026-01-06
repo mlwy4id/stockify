3764a2fb0c95b086bc53b80d63c2f7e7dbdc07d6
mlwy4id <azmiimaulawiysaid67@gmail.com>
2026-01-07 06:06:05 +0800

    feat: add button and card

--- src/pages/Transactions/page.tsx
+++ src/pages/Transactions/page.tsx
@@ -1,10 +1,25 @@
-import { Spinner } from '@/components/ui/spinner';
 import PageLayout from '../layout/PageLayout';
+import { Button } from '@/components/ui/button';
+import { LuPlus } from 'react-icons/lu';
+import { Card, CardContent } from '@/components/ui/card';
+import TransactionForm from '@/components/form/TransactionForm';
 
 const TransactionsPage = () => {
   return (
-    <PageLayout title="Transactions">
-      <Spinner />
+    <PageLayout
+      title="Transactions"
+      button={
+        <Button className="bg-blue-600 hover:bg-blue-500">
+          <LuPlus />
+          Add Transaction
+        </Button>
+      }
+    >
+      <Card className="h-screen">
+        <CardContent className="h-full">
+          <TransactionForm />
+        </CardContent>
+      </Card>
     </PageLayout>
   );
 };
