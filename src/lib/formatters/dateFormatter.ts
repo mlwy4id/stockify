5c581fc13cc5b568b5ae8c65286e4ffca4be648a
mlwy4id <azmiimaulawiysaid67@gmail.com>
2026-02-08 18:46:04 +0800

    change date format

--- src/lib/formatters/dateFormatter.ts
+++ src/lib/formatters/dateFormatter.ts
@@ -1,5 +1,5 @@
 export const dateFormatter = (date: Date) => {
   const isoDate = new Date(date);
 
-  return isoDate.toLocaleDateString('en-EN', { day: '2-digit', month: '2-digit', year: 'numeric' });
+  return isoDate.toLocaleDateString('id-ID', { day: '2-digit', month: '2-digit', year: 'numeric' });
 };
