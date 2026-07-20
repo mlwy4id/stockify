4013fcf0b2d43f537aeff796aedf057f6f04192d
mlwy4id <azmiimaulawiysaid67@gmail.com>
2026-07-21 06:00:24 +0800

    feat: add delete category handler

--- backend/internal/http/category/category_handler.go
+++ backend/internal/http/category/category_handler.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	app "github.com/mlwy4id/stockify/internal/application/command/category"
+	vo "github.com/mlwy4id/stockify/internal/domain/values_object"
 )
 
 type CategoryHandler struct {
@@ -34,7 +35,6 @@ func (ch *CategoryHandler) Create(ctx *gin.Context) {
 	}
 
 	cmd := app.CreateCategoryCommand{Name: req.Name}
-
 	id, err := ch.createCategoryHandler.Handle(ctx.Request.Context(), cmd)
 
 	if err != nil {
@@ -42,5 +42,30 @@ func (ch *CategoryHandler) Create(ctx *gin.Context) {
 		return
 	}
 
-	ctx.JSON(http.StatusCreated, gin.H{"category_id": id})
+	ctx.JSON(http.StatusCreated, gin.H{
+		"message":     "new category created successfully",
+		"category_id": id,
+	})
+}
+
+func (ch *CategoryHandler) Delete(ctx *gin.Context) {
+	id, err := vo.ParseCategoryId(ctx.Param("id"))
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	cmd := app.DeleteCategoryCommand{Id: id}
+	err = ch.deleteCategoryHandler.Handle(ctx.Request.Context(), cmd)
+
+	if err != nil {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"message":     "category deleted successfully",
+		"category_id": id.Value(),
+	})
 }
