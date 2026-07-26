1a636e5d345fd08a440beb2c2b85aaf62ab7426e
mlwy4id <azmiimaulawiysaid67@gmail.com>
2026-07-27 06:38:37 +0800

    feat: implement interceptor for auth header

--- frontend/src/shared/lib/axios/axios.ts
+++ frontend/src/shared/lib/axios/axios.ts
@@ -1,8 +1,22 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "/api",
-  withCredentials: true,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 export default api;
